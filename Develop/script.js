d67ee9180c087fb8a232630347cddead2e170b1a
ellysaschottler <ellysastork@gmail.com>
2023-09-21 18:01:42 -0500

    wrote prompts to receive input on what kind of password is needed

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+let characterNumber = prompt("Choose a password character length between 8 and 128.", "");
+let lowercaseCharacter = confirm ("Include a minimum of 1 lowercase character?");
+let uppercaseCharacter = confirm ("Include a minimum of 1 uppercase character?");
+let numericCharacter = confirm ("Include a minimum of 1 numeric character?");
+let specialCharacter = confirm ("Include a minimum of 1 numeric character?");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
