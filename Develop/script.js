0f1f4e1b9dcad79917bf2df5544d5a5366a7e906
ellysaschottler <ellysastork@gmail.com>
2023-09-21 18:09:35 -0500

    added if statement to verify correct character number was entered

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,15 @@
 // Assignment code here
 let characterNumber = prompt("Choose a password character length between 8 and 128.", "");
+
+if (characterNumber < 8 || characterNumber > 128) {
+  alert("characterNumber is invalid, please indicate a number between 8-128");
+}
+
 let lowercaseCharacter = confirm ("Include a minimum of 1 lowercase character?");
 let uppercaseCharacter = confirm ("Include a minimum of 1 uppercase character?");
 let numericCharacter = confirm ("Include a minimum of 1 numeric character?");
 let specialCharacter = confirm ("Include a minimum of 1 numeric character?");
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
