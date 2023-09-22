1a995f0416386cf231971da786a2ac120e1925ab
ellysaschottler <ellysastork@gmail.com>
2023-09-22 10:49:08 -0500

    I split the generatePassword function into two parts and added validation to make sure the user requests the minimum parameters for a password

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,40 @@
 // Assignment code here
-let characterNumber = prompt("Choose a password character length between 8 and 128.", "");
 
-if (characterNumber < 8 || characterNumber > 128) {
-  alert("characterNumber is invalid, please indicate a number between 8-128");
+// when the user clicks the button, the prompts appear in a writePassword function
+
+// when the prompts finish, the password generates into the #password field
+
+// need to make a generatePassword function
+
+
+//The generatePassword function has two parts - one function for getting the number of characters and one function for indicating what variable types are to be included.
+
+function generatePassword(){
+      function getValidCharacterNumber(){
+        let characterNumber = prompt("Choose a password character length between 8 and 128.", "");
+
+        for (characterNumber < 8 || characterNumber > 128) {
+          alert("characterNumber is invalid, please indicate a number between 8-128");
+          getValidCharacterNumber();
+
+      }
+
+  }
+      function indicateCharacterTypes(){
+        let includeLowercaseCharacter = confirm ("Include a minimum of 1 lowercase character?");
+        let includeUppercaseCharacter = confirm ("Include a minimum of 1 uppercase character?");
+        let includeNumericCharacter = confirm ("Include a minimum of 1 numeric character?");
+        let includeSpecialCharacter = confirm ("Include a minimum of 1 numeric character?");
+
+        if (!includeLowercaseCharacter && !uppercaseCharacter && !numericCharacter && !specialCharacter) {
+          alert("Please select at least one character type to generate your password.");
+          indicateCharacterTypes();
+        }
+      }
+ 
 }
 
-let lowercaseCharacter = confirm ("Include a minimum of 1 lowercase character?");
-let uppercaseCharacter = confirm ("Include a minimum of 1 uppercase character?");
-let numericCharacter = confirm ("Include a minimum of 1 numeric character?");
-let specialCharacter = confirm ("Include a minimum of 1 numeric character?");
+
 
 
 // Get references to the #generate element
