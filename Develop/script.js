5bcbb233eb35882b4a28ad90f57e278c6423ae03
ellysaschottler <ellysastork@gmail.com>
2023-09-22 13:49:25 -0500

    Added more pseudocode to plan functionality of setting the parameters for the password we will be generating

--- Develop/script.js
+++ Develop/script.js
@@ -6,36 +6,51 @@
 
 // need to make a generatePassword function
 
-
 //The generatePassword function has two parts - one function for getting the number of characters and one function for indicating what variable types are to be included.
 
-function generatePassword(){
-      function getValidCharacterNumber(){
-        let characterNumber = prompt("Choose a password character length between 8 and 128.", "");
+function generatePassword() {
+  let characterNumber = getValidCharacterNumber();
 
-        for (characterNumber < 8 || characterNumber > 128) {
-          alert("characterNumber is invalid, please indicate a number between 8-128");
-          getValidCharacterNumber();
-
-      }
-
-  }
-      function indicateCharacterTypes(){
-        let includeLowercaseCharacter = confirm ("Include a minimum of 1 lowercase character?");
-        let includeUppercaseCharacter = confirm ("Include a minimum of 1 uppercase character?");
-        let includeNumericCharacter = confirm ("Include a minimum of 1 numeric character?");
-        let includeSpecialCharacter = confirm ("Include a minimum of 1 numeric character?");
-
-        if (!includeLowercaseCharacter && !uppercaseCharacter && !numericCharacter && !specialCharacter) {
-          alert("Please select at least one character type to generate your password.");
-          indicateCharacterTypes();
-        }
-      }
- 
+  // code that uses that number...
+  
 }
 
+function getValidCharacterNumber() {
+  let characterNumber = prompt(
+    "Choose a password character length between 8 and 128.",
+    "8"
+  );
+
+  if (characterNumber < 8 || characterNumber > 128) {
+    alert(
+      "characterNumber is an invalid number of characters, please indicate a number between 8-128"
+    );
+    return getValidCharacterNumber();
+  }
+
+  return characterNumber
+}
+
+function getValidCharacters() {
+  let possibleCharacters = "";
+  if  = confirm("Include lowercase characters?");
+  //append abcdefghijklmnopqrstuvwxyz
+  if  = confirm("Include uppercase characters?");
+  //append ABCDEFGHIJKLMNOPQRSTUVWXYZ
+  if = confirm("Include numeric characters?");
+  //append 0123456789
+  if = confirm("Include special characters?");
+  //append " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 
+
+  if getValidCharacters is empty
+    alert(
+      "Please select at least one character type to generate your password."
+    );
+    getValidCharacters();
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -46,7 +61,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
