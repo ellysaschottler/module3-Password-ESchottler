e1a8f41e6d1bda1d6ff7531377ad6e46937c557b
ellysaschottler <ellysastork@gmail.com>
2023-09-25 16:20:18 -0500

    cleaned up the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,77 +1,83 @@
 // Assignment code here
 
-
 function generatePassword() {
   let characterNumber = getValidCharacterNumber();
-  console.log(characterNumber)
-  let validCharacters = getValidCharacters()
-  
-  
+  let validCharacters = getValidCharacters();
+  let password = "";
 
-  let password = ""
-
-  for (let i= 0; i < characterNumber; i++) {
-    password = password + validCharacters.charAt(Math.floor(Math.random() *validCharacters.length));
-    console.log(password)
-  }
-  return password
-}
-
-function getValidCharacterNumber(){
-  let characterNumber = prompt("Choose a password character length between 8 and 128.", "8");
-  
-  if (characterNumber < 8 || characterNumber > 128) {
-      alert(
-        "This is an invalid number of characters."
+  for (let i = 0; i < characterNumber; i++) {
+    password =
+      password +
+      validCharacters.charAt(
+        Math.floor(Math.random() * validCharacters.length)
       );
-      return getValidCharacterNumber();
-
-  } else if (characterNumber >= 8 && characterNumber <=128){
-      alert (`Your password will be ${characterNumber} characters long.  Please answer the next questions to determine the character types in your password.`)
-  };
-
-  return characterNumber
+  }
+  return password;
 }
 
-function getValidCharacters () {
-    let lowercaseLetters
-    let uppercaseLetters
-    let numericCharacters
-    let specialCharacters
-    if (confirm("Include lowercase letters?") == true) {
-      lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    } else {
-      lowercaseLetters = '';
-    }
-    
-    if (confirm ("Include uppercase letters?") == true) {
-      uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else {
-      uppercaseLetters = '';
-    }
+function getValidCharacterNumber() {
+  let characterNumber = prompt(
+    "Choose a password character length between 8 and 128.",
+    "8"
+  );
 
-    if (confirm ("Include numeric characters?") == true) {
-      numericCharacters = "0123456789";
-    } else {
-      numericCharacters = '';
-    }
+  if (characterNumber < 8 || characterNumber > 128) {
+    alert("This is an invalid number of characters.");
+    return getValidCharacterNumber();
+  } else if (characterNumber >= 8 && characterNumber <= 128) {
+    alert(
+      `Your password will be ${characterNumber} characters long.  Please answer the next questions to determine the character types in your password.`
+    );
+  }
 
-    if (confirm ("Include special characters?") == true) {
-      specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    } else {
-      specialCharacters = '';
-    }
-
-    if (lowercaseLetters + uppercaseLetters + numericCharacters + specialCharacters == "") {
-      alert ("Please choose at least one character type!")
-      return getValidCharacters()
-    }
-
-    return lowercaseLetters + uppercaseLetters + numericCharacters + specialCharacters;
+  return characterNumber;
 }
 
+function getValidCharacters() {
+  let lowercaseLetters;
+  let uppercaseLetters;
+  let numericCharacters;
+  let specialCharacters;
 
+  if (confirm("Include lowercase letters?") == true) {
+    lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    lowercaseLetters = "";
+  }
 
+  if (confirm("Include uppercase letters?") == true) {
+    uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else {
+    uppercaseLetters = "";
+  }
+
+  if (confirm("Include numeric characters?") == true) {
+    numericCharacters = "0123456789";
+  } else {
+    numericCharacters = "";
+  }
+
+  if (confirm("Include special characters?") == true) {
+    specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  } else {
+    specialCharacters = "";
+  }
+
+  if (
+    lowercaseLetters +
+      uppercaseLetters +
+      numericCharacters +
+      specialCharacters ==
+    ""
+  ) {
+    alert("Please choose at least one character type!");
+    return getValidCharacters();
+  }
+
+  return (
+    lowercaseLetters + uppercaseLetters + numericCharacters + specialCharacters
+  );
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
